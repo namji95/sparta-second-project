7efc8ecb64e64cb6bb7fa96ab9e27f2d89317b0f
namji <skawlgns07@naver.com>
2024-01-16 10:17:03 +0900

    병합 pull 받기 위한 commit

--- src/domain/StudentCreateRead.java
+++ src/domain/StudentCreateRead.java
@@ -4,33 +4,53 @@ import console.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StudentCreateRead {
+
+    private static long accountIdCounter = 0;
 
     // 학생 데이터들을 계속 하고 싶다면 while문으로 감싸세요
     public static Student StudentCreate () {
         Student newStudent = null;
-        while (true) {
-            System.out.println("학생 이름을 입력하세요");
-            String studentName = Console.inputString();
-            if (studentName.equalsIgnoreCase("end")) {
-                break;
+        boolean studentInfoSave = true;
+        while (studentInfoSave) {
+
+            String studentsName = studentName();
+            if (studentsName.equalsIgnoreCase("exit")) {
+                studentInfoSave = false;
+                continue;
             }
 
             List<Course> selectCourses = selectCourse();
 
             long countId = countIdCreate();
 
-            newStudent = new Student(countId, studentName, selectCourses);
+            newStudent = new Student(countId, studentsName, selectCourses);
             StudentData.getInstance().addStudent(newStudent);
 
             System.out.println("학생이 성공적으로 생성되었습니다!\n");
-
         }
         return newStudent;
     }
 
-    private  static List<Course> selectCourse() {
+    private static String studentName () {
+        System.out.println("이름을 입력하세요:");
+        String studentName = null;
+        boolean studentNameSave = true;
+        while (studentNameSave) {
+            studentName = Console.inputString();
+            if (Pattern.matches("^[가-힣]*$", studentName) == true ||
+                studentName.equalsIgnoreCase("exit")) {
+                studentNameSave = false;
+            } else {
+                System.out.println("한글로 다시 입력해주세요:");
+            }
+        }
+        return studentName;
+    }
+
+    private static List<Course> selectCourse() {
 
         System.out.println("선택 가능 과목 목록");
         for (CourseList selectCourseList : CourseList.values()) {
@@ -38,7 +58,6 @@ public class StudentCreateRead {
                                 " - " +
                                 selectCourseList.getType());
         }
-        long accountCourseId = courseId();
         System.out.println("수강할 과목을 선택하세요 (하나 이상 선택, 종료는 'exit'):");
         List<Course> selectCourses = new ArrayList<>();
             while (true) {
@@ -69,7 +88,7 @@ public class StudentCreateRead {
     }
 
     private static long countIdCreate() {
-        return System.currentTimeMillis();
+        return ++accountIdCounter;
     }
 
     public static void studentRead() {
@@ -80,8 +99,10 @@ public class StudentCreateRead {
                 break;
             }
 
+            boolean studentFound = false;
+
             for (Student student : StudentData.getInstance().getStudents()) {
-                if (student.getName().equalsIgnoreCase(studentSearch)) {
+                if (student.getName().equals(studentSearch)) {
                     System.out.println("계정 ID: " + student.getAccountId());
                     System.out.println("이름: " + student.getName());
                     System.out.println("수강 과목:");
@@ -89,9 +110,14 @@ public class StudentCreateRead {
                     for (Course course : student.getMyCourse()) {
                         System.out.println("- " + course.getCourseName());
                     }
+                    studentFound = true;
                 }
             }
-                System.out.println("----------------------------------");
+            if (!studentFound) {
+                System.out.println(!studentFound);
+                System.out.println("일치하는 이름이 없습니다.");
+            }
+            System.out.println("----------------------------------");
         }
     }
 }
